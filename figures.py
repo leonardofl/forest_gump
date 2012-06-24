584555d225ee3ad604eaaf8fa9e10cc7a5bcac6d
Leonardo Leite <leonardofl87@gmail.com>
2012-06-24 00:19:58 -0300

    images are downloaded

--- figures.py
+++ figures.py
@@ -1,10 +1,77 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2012, Leonardo Leite
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import re
 import sys
 import urllib2
+import urllib
 from BeautifulSoup import BeautifulSoup
 
 BASE_URL = 'http://%s.wikipedia.org/wiki/%s'
 
 LANGUAGE = 'pt'
+
+import glob
+
+def get_image(word, wiki=False):
+    """Acha uma imagem para uma palavra
+    word: palavra em português
+    wiki: se wiki=True e imagem não existe nas pasta img, então baixa figura da wikipedia
+    return caminho para a imagem; None se palavra não tem imagem (ou se não achar na wikipedia)
+    """
+    folder = "resources/img/"
+    word = word.replace(" ", "_")
+    list = glob.glob("%s%s.*" % (folder, word))
+    if list:
+        return list[0]
+    else:
+        if wiki:
+            try:
+                img_url = from_wikipedia(word)
+            except:
+                return None
+            if img_url:
+                path = folder + word + _ext(img_url)
+                download(img_url, path)
+                return path
+        return None
+
+def _ext(path):
+    """Returns the extension of the file (including the dot)
+    path: it can be local or an URL, it doesn't matter
+    """
+    regex = '.*(\.[a-zA-Z]{3})'
+    res = re.match(regex, path)
+    if res:
+        return res.group(1)
+    else:
+        return ''
+
+def download(img_url, path):
+    """Downloads an image in the path
+    path: the absolute path to the image, including the image name
+    """
+    # todo extensão da imagem
+    request = urllib2.Request(img_url, headers={'User-Agent' : "Magic Browser"})
+    data = urllib2.urlopen(request).read()
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
 
 def from_wikipedia(word):
     """Returns the link of a figure in the verbet of 'word' (a string)
@@ -22,7 +89,14 @@ def from_wikipedia(word):
         return None
     soup = BeautifulSoup(site)
     img_src = soup.find("a", { "class" : "image" }).findChild('img').get('src')
-    return img_src.replace('//', '')
+    return 'http://' + img_src.replace('//', '')
 
 if __name__ == "__main__":
-    print from_wikipedia(sys.argv[1])
+    print get_image(sys.argv[1], True)
+#    print get_image("escola")
+#    print get_image("python")
+#    print get_image("maçã")
+#    print get_image("quinta e breja")
+#    print get_image("naotem")
+
+
